94e2f884af592ff7a229ee52f5aded8caff4f5ad
Bett <bethwel.kimutai@strathmore.edu>
2025-09-24 20:23:33 +0300

    Fixed the prod settings

--- quidpath_backend/settings/prod.py
+++ quidpath_backend/settings/prod.py
@@ -1,9 +1,21 @@
-# settings/dev.py
+# settings/prod.py
 from .base import *
-DEBUG = True
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+import os
+import dj_database_url
+
+DEBUG = False  # Best practice: keep False in prod
+
+# Allow Railway & Azure hosts
+ALLOWED_HOSTS = ['*']
 
 print("Using Production Settings")
 
-import os
-print("DATABASE_URL:", os.environ.get("DATABASE_URL"))
+# Database config
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    }
+else:
+    raise Exception("❌ DATABASE_URL not set! Cannot connect to DB.")
