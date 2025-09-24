fde8c88ccd8799b56082c618d0b3aac889caa9e3
Bett <bethwel.kimutai@strathmore.edu>
2025-09-24 15:42:00 +0300

    Fix- created a start file to be able to fix the deployment start issue

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo "Running migrations..."
+python manage.py migrate
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Starting Gunicorn..."
+gunicorn quidpath_backend.wsgi:application --bind 0.0.0.0:$PORT
