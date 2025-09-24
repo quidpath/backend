ae55ff24f5c7207fa4c62d47f3bc6ca5cc81b6ea
Bett <bethwel.kimutai@strathmore.edu>
2025-09-24 16:29:06 +0300

    Fix changed back my python3 in my start.sh to python

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 echo "Running migrations..."
-python3 manage.py migrate
+python manage.py migrate
 
 echo "Collecting static files..."
-python3 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 echo "Starting Gunicorn..."
 gunicorn quidpath_backend.wsgi:application --bind 0.0.0.0:$PORT
