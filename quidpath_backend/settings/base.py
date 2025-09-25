ba9cd9957f235354c1b2d4b89f5e86ab4ee79f30
Bett <bethwel.kimutai@strathmore.edu>
2025-09-25 11:18:12 +0300

    Fixed authentication in installed apps back to uppercase

--- quidpath_backend/settings/base.py
+++ quidpath_backend/settings/base.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt.token_blacklist",
-    "authentication",
+    "Authentication",
     "corsheaders",
     "OrgAuth",
     "Banking",
